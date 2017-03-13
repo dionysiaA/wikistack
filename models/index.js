e1eb4e5c72ebac3449483603aadc3b699f90526a
Jason <jason.miguel@gmail.com>
2017-03-13 11:22:20 -0400

    examples

--- models/index.js
+++ models/index.js
@@ -35,6 +35,39 @@ var Page = db.define('page', {
 // });
 // ...
 
+var Person = db.define('person', {
+	first_name: {
+		type: Sequelize.STRING
+	},
+	last_name: {
+		type: Sequelize.STRING
+	},
+	full_name: {
+		type: Sequelize.VIRTUAL,
+		get: function () {
+			return this.first_name + ' ' + this.last_name;
+		},
+		set: function (incomingName) {
+			var arr = incomingName.split(' ');
+			var first_name = arr[0];
+			var last_name = arr[1];
+			this.first_name = first_name;
+			this.last_name = last_name;
+		}
+	}
+});
+
+// Getters and Setters example
+// Person.findOne({where: {last_name: 'Smith'}})
+// .then(function (person) {
+// 	console.log(person.full_name);
+// 	person.full_name = 'Omri Bernstein';
+// 	return person.save();
+// })
+// .then(function (changedPerson) {
+// 	console.log(changedPerson); //
+// });
+
 var User = db.define('user', {
 	name: {
 		type: Sequelize.STRING,
