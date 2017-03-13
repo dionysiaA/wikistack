ed71e24549e80423f7e101e53cb8fae020e39709
Jason <jason.miguel@gmail.com>
2017-03-13 11:05:59 -0400

    finished sequelize essentials

--- models/index.js
+++ models/index.js
@@ -3,26 +3,49 @@ var db = new Sequelize('postgres://localhost:5432/wikistack', {logging: false});
 
 var Page = db.define('page', {
 	title: {
-		type: Sequelize.STRING
+		type: Sequelize.STRING,
+		allowNull: false
 	},
 	urlTitle: {
-		type: Sequelize.STRING
+		type: Sequelize.STRING,
+		allowNull: false,
 	},
 	content: {
-		type: Sequelize.TEXT
+		type: Sequelize.TEXT, allowNull: false
 	},
 	status: {
 		type: Sequelize.ENUM('open', 'closed')
+	},
+	date: {
+		type: Sequelize.DATE, defaultValue: Sequelize.NOW
+	},
+	route: {
+		type: Sequelize.VIRTUAL,
+		get: function () {
+			return '/wiki/' + this.getDataValue('urlTitle');
+		}
 	}
 
 });
 
+// ...
+// Page.findOne({where: {title: 'Large Sheep'}})
+// .then(function (page) {
+// 	console.log(page.route); // '/wiki/Large_Sheep'
+// });
+// ...
+
 var User = db.define('user', {
 	name: {
-		type: Sequelize.STRING
+		type: Sequelize.STRING,
+		allowNull: false
 	},
 	email: {
-		type: Sequelize.STRING
+		type: Sequelize.STRING,
+		allowNull: false,
+		validate: {
+			isEmail: true
+		}
 	}
 });
 
